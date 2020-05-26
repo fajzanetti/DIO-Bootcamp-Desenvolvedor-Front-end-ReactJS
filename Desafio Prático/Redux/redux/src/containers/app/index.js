bc878de9d6eac215c37808c66d37e41b1251a9ad
Felipe A. Justino Zanetti <felipeajz@hotmail.com>
2020-05-26 15:43:42 -0300

    import component and setState

--- Desafio Prático/Redux/redux/src/containers/app/index.js
+++ Desafio Prático/Redux/redux/src/containers/app/index.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 
+import Button from '../../components/button'
+import Input from '../../components/input'
+import List from '../../components/list'
+
 export default class index extends Component {
     state = {
         input: ''
     }
 
+    handleOnClick = () => {
+        console.log('BUTTON')
+    }
+
+    handleOnChange = (event) => {
+        console.log('INPUT', event.target.value)
+    }
+
     render() {
         return (
-            <h1>APP</h1>
+            <div>
+                <List todolist={[]}/>
+                <Input onChange={(event) => this.handleOnChange(event)}/>
+                <Button onClick={this.handleOnClick}>Adicionar</Button>
+            </div>
         )
     }
 }
